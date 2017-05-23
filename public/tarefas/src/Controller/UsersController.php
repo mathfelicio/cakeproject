795ee0b9a7ddd7b6a65ae127d4b5d5cad9721607
luciano <dev02@grupog2i.com.br>
2017-05-23 17:49:13 -0400

    Adição dos métodos de login e logout para a aplicação

--- public/tarefas/src/Controller/UsersController.php
+++ public/tarefas/src/Controller/UsersController.php
@@ -110,4 +110,20 @@ class UsersController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
+    
+    public function login()
+    {
+    	if ($this->request->is('post')) {
+    		$user = $this->Auth->identify();
+    		if ($user) {
+    			$this->Auth->setUser($user);
+    			return $this->redirect($this->Auth->redirectUrl());
+    		}
+    	}
+    }
+
+    public function logout()
+    {
+        return $this->redirect($this->Auth->logout());
+    }
 }
